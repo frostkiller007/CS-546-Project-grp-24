6f694aceef0b270b2a48973f0140178cd21a2a89
aditya214a <adityaadhduk1@gmail.com>
2022-12-15 13:42:48 -0500

    added mainPage routes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const userRoutes = require("./user");
 const postRoutes = require("./post");
 const chatRoutes = require("./chats");
 const commentRoutes = require("./comment");
-const homepageRoutes = require("./homepage");
+const mainPageRoutes = require("./mainPage");
 const reportRoutes = require("./report");
 
 const constructorMethod = (app) => {
-  app.use("/homepage", homepageRoutes);
+  app.use("/mainPage", mainPageRoutes);
   app.use("/user", userRoutes);
   app.use("/post", postRoutes);
   app.use("/chat", chatRoutes);
@@ -14,7 +14,7 @@ const constructorMethod = (app) => {
   app.use("/comment", commentRoutes);
 
   app.get("/", (req, res) => {
-    res.redirect("http://localhost:3000/homepage");
+    res.redirect("http://localhost:3000/mainPage");
   });
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Not found" });
---
+++ routes/report.js
@@ -0,0 +1,1 @@
+const reportData = data.report;

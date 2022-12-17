8f54eafcbbfb12b0a3ac1fa1a88457fcc289b810
aditya214a <adityaadhduk1@gmail.com>
2022-12-17 13:43:09 -0500

    report error

--- routes/report.js
+++ routes/report.js
@@ -2,58 +2,33 @@ const express = require("express");
 const router = express.Router();
 router.route("/").get(async (req, res) => {});
 
-
-router.get("/form" , async (req, res) => {
-    if (!req.session.userId) {   
-      return res.redirect('/homePage');
-    }
-    try{
-      const ID = req.session.userId;
-      const login = await userData.getUserById(ID);
-      const postId = req.query.id;
-      const post = await postData.getPostById(postId);
-      res.render('reports/report-form',{login,'reported-post':post.topic, 'postId': postId});
-    }catch(e){
-      res.status(404).json({ error: e });
-    }
-  });
-  router.post("/form", async (req, res) => {
-    const Id = req.session.userId;
-    const login = await userData.getUserById(Id);
-    const postId=req.body.postId;
+router.get("/form", async (req, res) => {
+  if (!req.session.userId) {
+    return res.redirect("/homePage");
+  }
+  try {
+    const userId = req.session.userId;
+    const userLogin = await userData.getUserById(userId);
+    const postId = req.query.id;
     const post = await postData.getPostById(postId);
-      try
-      {
-          let reason = req.body.reason;
-          if(typeof reason == "string")
-          {
-            reason=[reason];
-          }
-          await reportData.addReport(Id,postId,reason);
-          res.render('reports/report-form',{success:"Report successfully submitted!", login,'reported-post':post.topic, 'postId': postId});
-          return;
-      }
-      catch(e)
-      {
-        res.render('reports/report-form',{message:e, login,'reported-post':post.topic, 'postId': postId});
-      } 
-  });
-  
-  router.get('/statistic', async (req, res) => {
-    if (req.session && req.session.userId) {
-      let login = await userData.getUserById(req.session.userId)
-      if (login.Admin) {
-        try {
-          let allPosts = await postData.getAllPost();
-          let allUsers = await userData.getAllUsers();
-          let allComments = await commentData.getAllComments();
-          res.render('statistics/statistics',{ allPosts,allUsers, allComments,login });
-        } catch (error) {
-          res.status(404).send(error);
-        }
-      }
-      else
-        res.redirect('/homePage');
+    res.render("reports/report-form", {
+      userLogin,
+      "reported-post": post.topic,
+      postId: postId,
+    });
+  } catch (e) {
+    res.status(404).json({ error: e });
+  }
+});
+router.post("/form", async (req, res) => {
+  const userId = req.session.userId;
+  const userLogin = await userData.getUserById(userId);
+  const postId = req.body.postId;
+  const post = await postData.getPostById(postId);
+  try {
+    let reason = req.body.reason;
+    if (typeof reason == "string") {
+      reason = [reason];
     }
     await reportData.addReport(userId, postId, reason);
     res.render("reports/report-form", {
@@ -90,18 +65,28 @@ router.get("/statistic", async (req, res) => {
       } catch (error) {
         res.status(404).send(error);
       }
-    });
-  
-  router.get("/", async (req, res) => {
-    if (req.session && req.session.userId) {
-      let login = await userData.getUserById(req.session.userId);
-      if (login.Admin) {
-        const r_list= await reportData.getAllReports();
-        try {
-          res.render('reports/r_list',{r_list,login});
-        } catch (error) {
-          res.status(404).send(error);
-        }
+    } else res.redirect("/homePage");
+  } else res.redirect("/homePage");
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const report = await reportData.getReport(req.params.id);
+    res.status(200).json(report);
+  } catch (e) {
+    res.status(404).json({ message: "Report not found" });
+  }
+});
+
+router.get("/", async (req, res) => {
+  if (req.session && req.session.userId) {
+    let userLogin = await userData.getUserById(req.session.userId);
+    if (userLogin.Admin) {
+      try {
+        const reportList = await reportData.getAllReports();
+        res.render("reports/reportList", { reportList, userLogin });
+      } catch (error) {
+        res.status(404).send(error);
       }
     } else res.redirect("/homePage");
   } else res.redirect("/homePage");

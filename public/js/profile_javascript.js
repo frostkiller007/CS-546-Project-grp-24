c1775a168d19f79d4a7ad82a158bd8153d3850b9
NEEL1919 <neelvirani2001@student.sfit.ac.in>
2022-12-17 13:46:38 -0500

    Profile pic change

--- public/js/profile_javascript.js
+++ public/js/profile_javascript.js
@@ -1,5 +1,4 @@
 var loadFile = function (event) {
-    var image = document.getElementById("output");
-    image.src = URL.createObjectURL(event.target.files[0]);
-  };
-  
+  var image = document.getElementById("output");
+  image.src = URL.createObjectURL(event.target.files[0]);
+};
